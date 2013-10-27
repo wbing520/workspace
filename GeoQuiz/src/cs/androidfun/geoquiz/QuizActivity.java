62a23b14ed321e1cbf2c66b58437e7a423e0199f
weibing <wbing520@gmail.com>
2013-10-27 17:00:14 -0600

    Add the new Android simple project
    Project1. Helloworld to verify the working environment is correct.
    Project 2. GeoQuiz project using button and Toast.

---
+++ GeoQuiz/src/cs/androidfun/geoquiz/QuizActivity.java
@@ -0,0 +1,50 @@
+package cs.androidfun.geoquiz;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class QuizActivity extends Activity {
+
+	private Button mTrueButton;
+	private Button mFalseButton;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+        
+        mTrueButton = (Button)findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Toast.makeText(getApplicationContext(), R.string.true_toast, Toast.LENGTH_SHORT).show();
+			}
+		});
+        
+        mFalseButton =(Button)findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Toast.makeText(getApplicationContext(), R.string.false_toast, Toast.LENGTH_SHORT).show();
+				
+			}
+		});
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.quiz, menu);
+        return true;
+    }
+    
+}
