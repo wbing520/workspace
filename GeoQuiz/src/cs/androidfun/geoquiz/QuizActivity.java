25617d1662574eccb5c37626115487e0e7fb9d49
weibing <wbing520@gmail.com>
2013-10-28 12:01:18 -0600

    GeoQuiz new Feature
    1.  Add new activity layout to support rotation correctly.
    2. Override the onSaveInstanceState() method to passed the value between
    different orientation.

--- GeoQuiz/src/cs/androidfun/geoquiz/QuizActivity.java
+++ GeoQuiz/src/cs/androidfun/geoquiz/QuizActivity.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 public class QuizActivity extends Activity {
 
 	private static final String TAG = "QuizActivity";
+	private static final String KEY_INDEX = "index";
 	
 	private Button mTrueButton;
 	private Button mFalseButton;
@@ -66,7 +67,17 @@ public class QuizActivity extends Activity {
 			}
 		});
         
+        if(savedInstanceState != null){
+        	mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
         updateQuestion();
+    }
+    
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+    	super.onSaveInstanceState(savedInstanceState);
+    	Log.i(TAG, "onSaveInstanceState");
+    	savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
     }
     
   
