1da67bc73fd04f560d31deaaa05fecebdbccbf39
weibing <wbing520@gmail.com>
2013-10-27 22:11:29 -0600

    Add the Object (Model) in GeoQuiz Project
    To make the MVC architecture more clearly.

--- GeoQuiz/gen/cs/androidfun/geoquiz/R.java
+++ GeoQuiz/gen/cs/androidfun/geoquiz/R.java
@@ -24,9 +24,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int false_button=0x7f080001;
-        public static final int true_button=0x7f080000;
+        public static final int action_settings=0x7f080004;
+        public static final int false_button=0x7f080002;
+        public static final int next_button=0x7f080003;
+        public static final int question_text_view=0x7f080000;
+        public static final int true_button=0x7f080001;
     }
     public static final class layout {
         public static final int activity_quiz=0x7f030000;
@@ -37,11 +39,14 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int false_button=0x7f050004;
-        public static final int false_toast=0x7f050006;
-        public static final int question_text=0x7f050002;
-        public static final int true_button=0x7f050003;
-        public static final int true_toast=0x7f050005;
+        public static final int false_button=0x7f050003;
+        public static final int false_toast=0x7f050005;
+        public static final int next_button=0x7f050006;
+        public static final int question_asia=0x7f050009;
+        public static final int question_oceans=0x7f050007;
+        public static final int question_place=0x7f050008;
+        public static final int true_button=0x7f050002;
+        public static final int true_toast=0x7f050004;
     }
     public static final class style {
         /** 
--- GeoQuiz/src/cs/androidfun/geoquiz/QuizActivity.java
+++ GeoQuiz/src/cs/androidfun/geoquiz/QuizActivity.java
@@ -5,17 +5,30 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends Activity {
 
 	private Button mTrueButton;
 	private Button mFalseButton;
+	private Button mNextButton;
+	private TextView mQuestionTextView;
 	
+	private TrueFalse[] mQuestionBank = new TrueFalse[]{
+			new TrueFalse(R.string.question_oceans, true),
+			new TrueFalse(R.string.question_asia,true),
+			new TrueFalse(R.string.question_place,false),
+	};
+	
+	private int mCurrentIndex=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        
+        mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
+
         
         mTrueButton = (Button)findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
@@ -23,7 +36,8 @@ public class QuizActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Toast.makeText(getApplicationContext(), R.string.true_toast, Toast.LENGTH_SHORT).show();
+				//Toast.makeText(getApplicationContext(), R.string.true_toast, Toast.LENGTH_SHORT).show();
+				checkAnswer(true);
 			}
 		});
         
@@ -33,10 +47,42 @@ public class QuizActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Toast.makeText(getApplicationContext(), R.string.false_toast, Toast.LENGTH_SHORT).show();
-				
+				//Toast.makeText(getApplicationContext(), R.string.false_toast, Toast.LENGTH_SHORT).show();
+				checkAnswer(false);
 			}
 		});
+        
+        mNextButton=(Button) findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				mCurrentIndex =(mCurrentIndex + 1) % mQuestionBank.length;
+				updateQuestion();
+			}
+		});
+        
+        updateQuestion();
+    }
+    
+  
+    private void updateQuestion(){
+    	int question = mQuestionBank[mCurrentIndex].getQuestion();
+    	mQuestionTextView.setText(question);
+    }
+    
+    private void checkAnswer(boolean userPressedTrue){
+    	boolean answerIsTrue= mQuestionBank[mCurrentIndex].isTrueQuestion();
+    	
+    	int messageResId= 0;
+    	if(userPressedTrue == answerIsTrue){
+    		messageResId = R.string.true_toast;    		
+    	}else{
+    		messageResId = R.string.false_toast;
+    	}
+    	
+    	Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
     }
 
 
