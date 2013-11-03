9917bc3810331428b27ab510fbb20918a8a20e24
weibing <wbing520@gmail.com>
2013-11-02 22:32:45 -0600

    CriminalIntent New Features
    1. add new widgets: linearlayout, button, edittext, checkbox
    2. add the land layout

--- CriminalIntent/src/cs/androidfun/criminalintent/Crime.java
+++ CriminalIntent/src/cs/androidfun/criminalintent/Crime.java
@@ -1,14 +1,18 @@
 package cs.androidfun.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
 
 	private UUID mId;
 	private String mTitle;
+	private Date mDate;
+	private boolean mSolved;
 	
 	public Crime(){
 		mId = UUID.randomUUID();
+		mDate = new Date();
 	}
 
 	public String getTitle() {
@@ -22,7 +26,21 @@ public class Crime {
 	public UUID getId() {
 		return mId;
 	}
-	
-	
+
+	public Date getDate() {
+		return mDate;
+	}
+
+	public void setDate(Date date) {
+		mDate = date;
+	}
+
+	public boolean isSolved() {
+		return mSolved;
+	}
+
+	public void setSolved(boolean solved) {
+		mSolved = solved;
+	}
 }
 
--- CriminalIntent/src/cs/androidfun/criminalintent/CrimeFragment.java
+++ CriminalIntent/src/cs/androidfun/criminalintent/CrimeFragment.java
@@ -7,12 +7,18 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
 
 public class CrimeFragment extends Fragment {
 
 	private Crime mCrime;
 	private EditText mTitleField;
+	private Button mDateButton;
+	private CheckBox mSolvedCheckBox;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
@@ -23,6 +29,7 @@ public class CrimeFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState){
 		View v = inflater.inflate(R.layout.fragement_crime, parent, false);
+		
 		
 		mTitleField = (EditText) v.findViewById(R.id.crime_title);
 		mTitleField.addTextChangedListener(new TextWatcher(){
@@ -44,6 +51,16 @@ public class CrimeFragment extends Fragment {
 			}
 		});
 		
+		mDateButton = (Button) v.findViewById(R.id.crime_date);
+		mDateButton.setText(mCrime.getDate().toString());
+		mDateButton.setEnabled(false);
+		
+		mSolvedCheckBox =(CheckBox)v.findViewById(R.id.crime_solved);
+		mSolvedCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener(){
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
+				mCrime.setSolved(isChecked);
+			}
+		});
 		return v;		
 		}
 }
