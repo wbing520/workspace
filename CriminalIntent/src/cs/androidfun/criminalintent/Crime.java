be9d2d2058c2beec8c3025d885f5e9c8352d45f1
weibing <wbing520@gmail.com>
2013-11-07 21:51:40 -0700

    CriminalIntent New Features
    1. use ArrayAdapter to show the array of crimes
    2. still show Fragment on Activity to show the adapter
    3. add the log to show the clicked item#

--- CriminalIntent/src/cs/androidfun/criminalintent/Crime.java
+++ CriminalIntent/src/cs/androidfun/criminalintent/Crime.java
@@ -15,6 +15,11 @@ public class Crime {
 		mDate = new Date();
 	}
 
+	@Override
+	public String toString(){
+		return mTitle;
+	}
+	
 	public String getTitle() {
 		return mTitle;
 	}
---
+++ CriminalIntent/src/cs/androidfun/criminalintent/CrimeListActivity.java
@@ -0,0 +1,13 @@
+package cs.androidfun.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+
+	@Override
+	protected Fragment createFragment() {
+		// TODO Auto-generated method stub
+		return new CrimeListFragment();
+	}
+
+}
